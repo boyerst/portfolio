39cd50e8b5adc612f95e00bfac54d85720e3a4cf
Steve Boyer <stephen.a.boyer@gmail.com>
2022-11-30 20:38:58 -0500

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,26 @@ module.exports = {
     'react/static-property-placement': 'off', // disable if necessary
     'react/jsx-props-no-spreading': 'off', // disable if necessary
     'react/require-default-props': 'off',
+    "quotes": [1, "double"],
+    "quote-props": "off",
+    "semi": [0, "never"],
+    "padded-blocks": ["error", { "classes": "always" }],
+    "react/destructuring-assignment": "off",
+    "comma-dangle": "off",
+    "operator-linebreak": "off",
+    "no-multiple-empty-lines": "off",
+    "react/jsx-one-expression-per-line": "off",
+    "no-nested-ternary": "off",
+    "no-shadow": "off",
+    "object-curly-newline": [1, { "ObjectPattern": { "multiline": true } }],
+    "arrow-body-style": "off",
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/jsx-wrap-multilines": "off",
+    "react/prop-types": 0,
+    "react/jsx-props-no-spreading": 0,
+    "react/function-component-definition": [2, { "namedComponents": "arrow-function" }],
+    "react/jsx-no-bind": [0, { "allowBind": true }]
+
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
