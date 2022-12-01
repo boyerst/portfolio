2719e79caccb840a44be3cad97adba9d6887ef41
Steve Boyer <stephen.a.boyer@gmail.com>
2022-12-01 18:15:05 -0500

    Clean up Tech component

--- src/components/Tech/Tech.jsx
+++ src/components/Tech/Tech.jsx
@@ -41,7 +41,7 @@ const Tech = () => {
                       delay={1000}
                       distance="30px"
                     >
-                      <div className="technology-wrapper__image">
+                      <div className="tech-wrapper__image">
                         <Tilt
                           options={{
                             reverse: false,
@@ -66,88 +66,6 @@ const Tech = () => {
               })}
               </Row>
             </Container>
-        </div>
-      </Container>
-
-
-
-
-      <Container>
-        <div className="tech-wrapper">
-          <Title title="Tech" />
-          {tech.map((stack) => {
-            const { id, img, title, info, info2, url } = stack;
-
-            return (
-              <Row key={id}>
-                <Col lg={4} sm={12}>
-                  <Fade
-                    left={isDesktop}
-                    bottom={isMobile}
-                    duration={1000}
-                    delay={500}
-                    distance="30px"
-                  >
-                    <div className="tech-wrapper__text">
-                      <h3 className="tech-wrapper__text-title">{title || 'Technology'}</h3>
-                      <div>
-                        <p>
-                          {info ||
-                            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
-                        </p>
-                        <p className="mb-4">{info2 || ''}</p>
-                      </div>
-                      <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="cta-btn cta-btn--hero"
-                        href={url || '#!'}
-                      >
-                        link here?
-                      </a>
-
-                    </div>
-                  </Fade>
-                </Col>
-                <Col lg={8} sm={12}>
-                  <Fade
-                    right={isDesktop}
-                    bottom={isMobile}
-                    duration={1000}
-                    delay={1000}
-                    distance="30px"
-                  >
-                    <div className="technology-wrapper__image">
-                      <a
-                        href={url || '#!'}
-                        target="_blank"
-                        aria-label="Technology Link"
-                        rel="noopener noreferrer"
-                      >
-                        <Tilt
-                          options={{
-                            reverse: false,
-                            max: 8,
-                            perspective: 1000,
-                            scale: 1,
-                            speed: 300,
-                            transition: true,
-                            axis: null,
-                            reset: true,
-                            easing: 'cubic-bezier(.03,.98,.52,.99)',
-                          }}
-                        >
-                          <div data-tilt className="thumbnail rounded">
-                            <ProjectImg alt={title} filename={img} />
-                          </div>
-                        </Tilt>
-                      </a>
-                    </div>
-                  </Fade>
-                </Col>
-              </Row>
-            );
-          })}
         </div>
       </Container>
     </section>
